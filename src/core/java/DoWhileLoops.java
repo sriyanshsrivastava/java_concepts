0a658fb9a5dccc4cee88d0d8747c0c8346ffd133
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-09 00:32:36 +0530

    do-while loop

---
+++ src/core/java/DoWhileLoops.java
@@ -0,0 +1,14 @@
+package core.java;
+
+public class DoWhileLoops {
+    public static void main(String[] args) {
+        int i = 5;
+
+        do
+        {
+            System.out.println("Hi "+i);
+            i++;
+        }while(i<=4);
+
+    }
+}
