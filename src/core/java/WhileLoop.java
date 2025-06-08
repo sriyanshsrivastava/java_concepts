d1b421598b71d64f2943b95c69cac6aae25a9e5b
Sriyansh Srivastava <23f3004255@ds.study.iitm.ac.in>
2025-06-09 00:06:50 +0530

    while loop, nested-while loop

--- src/core/java/WhileLoop.java
+++ src/core/java/WhileLoop.java
@@ -12,22 +12,20 @@ public class WhileLoop {
 //
 //        System.out.println("Bye "+ i);
 
-        int i = 1;                       //counter
-        while(i<=5)                      // check condition
+        int i = 1;                          //counter
+        while(i<=5)                         // check condition
         {
-            System.out.println("Hi "+i); // Do something as you want
+            System.out.println("Hi "+i);    // Do something as you want
             int j =1;                       // another counter variable for inner while loop
             while(j<=3){                    //nested while loop
                 System.out.println("Hello "+j);
                 j++;
             }
 
-            i++;                         //increment the value of i by 1 after every loop
+            i++;                             //increment the value of i by 1 after every loop
         }
 
         System.out.println("Bye "+ i);
-
-
 
 
     }
