32179dd6063824e350c2bf03163fee09fe7c4d35
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-10 17:15:03 +0530

    Adding Anonymous Object concepts

--- src/oops/java/AnonymousObject.java
+++ src/oops/java/AnonymousObject.java
@@ -12,7 +12,13 @@ class Anonymous{
 
 public class AnonymousObject {
     public static void main(String[] args) {
-        Anonymous obj = new Anonymous();
-        obj.show();
+        Anonymous obj = new Anonymous(); // here obj is not the Object in reality its reference variable for the object
+        obj.show();  // method of Anonymous class object
+
+        new Anonymous(); // this is anonymous object which have no any real reference variable
+        new Anonymous().show(); // this work, but we can not access it same object again
     }
 }
+/* object without reference variable is known as
+    anonymous object; we can not access it twice only one time we can access it
+ */
