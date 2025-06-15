5fd4b2756f6c7184f848f88ff5df483f445feb08
Sriyansh Srivastava <sriyanshsrivastava99@gmail.com>
2025-06-16 00:52:19 +0530

    adding Inner class concepts

--- src/oops/java/InnerClass.java
+++ src/oops/java/InnerClass.java
@@ -42,5 +42,35 @@ package oops.java;
  */
 
 
+// Outer class Creation
+class Outer{
+    int age;
+    public void show(){
+        System.out.println("In outer show");
+    }
+
+    // Inner Class into Outer class, means Inner class is inside the outer class.
+    // we can make this class static.
+    //static class Inner{  // we can make inner class as static class.
+    class Inner{
+        public void config(){
+            System.out.println("in Inner config");
+        }
+    }
+}
+// static class can only be created inside any class. Outer class static will not work it will throw error.
+
+
 public class InnerClass {
+    public static void main(String[] args) {
+        Outer obj = new Outer();
+        obj.show();
+        // Inner obj = new Inner(); // we can not do this here we can't create a object of inner class directly.
+        // Outer.Inner obj1 = new Inner(); // this will also don't work
+        Outer.Inner obj1 = obj.new Inner(); // so to create object of Inner class we need to have object of Outer class.
+        obj1.config(); // this way we can access the config() method of Inner class.
+
+        Outer.Inner obj2; // we can declare this as type of the class but we have to access outer first and then inner.
+
+    }
 }
